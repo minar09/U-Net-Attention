17915825e4219dfa74056eb8241932f68bd2abff
Matiur Rahman Minar <minar09.bd@gmail.com>
2019-03-07 12:47:46 +0900

    Back to basic U-Net! Pre-basic!

--- bfscore.py
+++ bfscore.py
@@ -14,12 +14,7 @@ bDebug = False
 
 
 def init_path():
-    # data_dir = 'logs/FCN_LIP/TestImage/'
-    # data_dir = 'logs/FCN_10k/TestImage/'
-    data_dir = 'logs/FCN_CFPD/TestImage/'
-    # data_dir = 'logs/UNet_LIP/TestImage/'
-    # data_dir = 'logs/UNet_10k/TestImage/'
-    # data_dir = 'logs/UNet_CFPD/TestImage/'
+    # data_dir = 'logs/10k/TestImage/'
 
     val_gt_paths = []
     val_pred_paths = []
